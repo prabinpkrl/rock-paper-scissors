55242ce22babcb0fd54689f942d73cdfa172d06e
Prabin Pokhrel <prabinpokhrel7722@gmail.com>
2024-01-09 22:26:04 +0545

    update html

--- index.js
+++ index.js
@@ -1,57 +1,56 @@
+const Choices=['rock', 'paper' ,'scissors'];
+const playerChoice=document.getElementById('playerselect');
+const computerChoice=document.getElementById('computerselect');
+const resultDisplay=document.getElementById('resultdisplay');
+const playerCount=document.getElementById('playercount');
+const computerCount=document.getElementById('computercount');
+const winOrLose=document.getElementById('winorlose');
+let playerScore=0;
+let computerScore=0;
 
 
-function getComputerChoice(){
-    const Choices=['rock', 'paper' ,'scissors'];
-    const randomSelect=Math.floor(Math.random()*Choices.length);
-    return Choices[randomSelect];
-}
-
-function playRound(playerSelection, computerSelection){
-     playerSelection = playerSelection.toLowerCase();
-
-    if(playerSelection === computerSelection){
-        return 'Its a tie!! Rematch';
+function playRound(playerSelect){
+    
+    const computerselect=Choices[(Math.floor(Math.random()*Choices.length))];
+    let result="";
+    if(playerSelect === computerselect){
+        result='Its a tie!! Rematch';
     }
-    else if(playerSelection==='rock' && computerSelection==='scissors'){
-        return 'player wins';
+    else if(playerSelect==='rock' && computerselect==='scissors'){
+        result='player wins';
+        playerScore++;
     }
-    else if(playerSelection==='paper' && computerSelection==='rock'){
-        return 'player wins';
+    else if(playerSelect==='paper' && computerselect==='rock'){
+        result='player wins';
+        playerScore++;
     }
-    else if(playerSelection==='scissors' && computerSelection==='paper'){
-        return 'player wins';
+    else if(playerSelect==='scissors' && computerselect==='paper'){
+        result='player wins';
+        playerScore++;
     }
     else{
-        return 'player lose!!';
+        result='player lose!!';
+        computerScore++;
+    }
+    
+    playerChoice.textContent=`PLAYER: ${playerSelect}`;
+    computerChoice.textContent=`COMPUTER: ${computerselect}`;
+    resultDisplay.textContent=result;
+    playerCount.textContent= 'Player Win:'+ playerScore;
+    computerCount.textContent= 'computer Win:'+ computerScore;
+
+    if(playerScore === 5){
+        winOrLose.textContent= 'Yay! YOU WIN';
+    }
+    else{
+        winOrLose.textContent= 'SAD! YOU LOSE';
     }
 
 }
 
 
-function game(){
-    let playerScore=0;
-    let computerScore=0;
 
-    for(let i=0; i<5; i++){
-        const playerSelection = prompt("Enter your:");
-        const computerSelection = getComputerChoice();
-        const roundResult= (playRound(playerSelection, computerSelection));
-        
-        if(roundResult.includes('win')){
-            playerScore++;
-        }
-        else if(roundResult.includes('lose')){
-            computerScore++;
-        }
-        console.log(roundResult);
-    }
-    if (playerScore > computerScore) {
-        console.log("Congratulations! You win the game.");
-      } else if (playerScore < computerScore) {
-        console.log("Sorry! You lose the game.");
-      } else {
-        console.log("It's a tie game.");
-      }
-}
-game();
+
+
+
 
